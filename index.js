29b4b9757f8b6df32fd4187156e7e67484197586
LazyTeenStudios <harleypeterson@gmail.com>
2023-10-01 14:13:00 -0500

    fixing path errors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.sendFile('./Main.html')
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
 app.listen(process.env.PORT || 3000)
