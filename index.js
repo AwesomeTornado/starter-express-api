4c9573d60bdcc9e8577c4a16db28609a9ead9344
LazyTeenStudios <harleypeterson@gmail.com>
2023-10-01 14:25:23 -0500

    corrected file name

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const path = require('path');
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/Main.html'));
 })
 app.listen(process.env.PORT || 3000)
