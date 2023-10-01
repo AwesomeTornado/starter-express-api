203dbbb8e31372dda166955a35c987a12a915da4
LazyTeenStudios <harleypeterson@gmail.com>
2023-10-01 14:10:27 -0500

    fix path name

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.sendFile('Main.html')
+    res.sendFile('./Main.html')
 })
 app.listen(process.env.PORT || 3000)
